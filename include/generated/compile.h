2674dba96dbd8b8442ebcd1044a3392602580b8e
Geoholt <sa.rt2034@ya.ru>
2019-05-23 18:04:48 +0300

    Add source

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#3 SMP PREEMPT Wed May 22 20:42:47 +03 2019"
+#define LINUX_COMPILE_BY "liklax"
+#define LINUX_COMPILE_HOST "liklax-hyper-pc"
+#define LINUX_COMPILER "gcc version 5.5.0 (Linaro GCC 5.5-2017.10) "
